3b2e5116ae36a8c06c6cdd52dc72777d1d871a5b
Krystian <226311@student.uek.krakow.pl>
2024-06-15 23:26:30 +0200

    Settings repaired

--- edenthought/settings.py
+++ edenthought/settings.py
@@ -18,9 +18,10 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["django-thoughts-app.onrender.com", "*"]
 
-# CSRF_TRUSTED_ORIGINS = []
+# FORM POST etc
+CSRF_TRUSTED_ORIGINS = ["https://django-thoughts-app.onrender.com"]
 
 
 # Application definition
